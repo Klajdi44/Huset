82f887feba20e25af2a1b77f72836b913fafff68
blanche82 <adelajda.kolodziejsk@gmail.com>
2019-04-26 16:10:40 +0200

    changes

--- javaScript/script.js
+++ javaScript/script.js
@@ -13,6 +13,27 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
+const pageTitle = document.getElementById("PageTitle");
+const eventFilters = document.getElementById("EventFilters");
+const eventfiltersContent = document.getElementById("FiltersContent");
+
+Array.from(document.getElementsByTagName('a')).forEach(element => {
+	element.onclick = function() { 
+		pageTitle.innerHTML = (element.innerHTML);
+		if (element.innerHTML != 'Events') {
+			eventFilters.style.display = 'none';
+		} else {
+			eventFilters.style.display = 'block';
+		}		
+		
+	};
+});
+
 function myFunction(){
-const burgermeny = document.getElementsByClassName("burgerMenu");
-burgermeny.classList.add('.slide-top ');}
+	const burgermeny = document.getElementsByClassName("burgerMenu");
+	burgermeny.classList.add('.slide-top ');
+}
+
+function showFilters() {
+	eventfiltersContent.classList.toggle('hidden');
+}
